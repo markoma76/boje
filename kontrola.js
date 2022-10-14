5935fc24a19882df0abae7b74828c0f7b2077cbe
markoma76 <marex999@gmail.com>
2022-10-14 09:28:42 +0200

    promena

--- kontrola.js
+++ kontrola.js
@@ -10,4 +10,7 @@ function slucaj(event) {
     boja = x * 0.0625;
 
     document.body.style = "background: linear-gradient(90deg, rgba(1,0,10,1) 0%, rgba(5,35,255,1) " + boja + "%, rgba(0,212,255,1) 100%)"
-}
+};
+
+let number = 666;
+let text = "proba"
