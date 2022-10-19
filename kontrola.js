dfeb9cbca309ef7682d6dc4c11932eb2cb93db21
markoma76 <marex999@gmail.com>
2022-10-19 08:49:31 +0200

    b

--- kontrola.js
+++ kontrola.js
@@ -11,3 +11,5 @@ function slucaj(event) {
 
     document.body.style = "background: linear-gradient(90deg, rgba(1,0,10,1) 0%, rgba(5,35,255,1) " + boja + "%, rgba(0,212,255,1) 100%)"
 }
+
+/* ovde vrsim novi unos podataka u verziju koja ne bi smela da bude promenjena ako se ja PerformanceNavigationTiming. Branch je grana a mi furamo working tree */
