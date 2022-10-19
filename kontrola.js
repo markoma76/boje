645804cc7f67364302e125ee5943ae6f24817ad9
markoma76 <marex999@gmail.com>
2022-10-19 08:46:47 +0200

    predhodno stanje

--- kontrola.js
+++ kontrola.js
@@ -11,6 +11,3 @@ function slucaj(event) {
 
     document.body.style = "background: linear-gradient(90deg, rgba(1,0,10,1) 0%, rgba(5,35,255,1) " + boja + "%, rgba(0,212,255,1) 100%)"
 };
-
-let number = 666;
-let text = "proba"
